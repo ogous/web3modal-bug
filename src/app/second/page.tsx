af889fed9f6c879b8af5c7f16bc994f41a9e5585
oguzhanali <oguzxan.ali@gmail.com>
2023-09-11 18:14:08 +0300

    fix: homepage link

--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Page() {
     return (
         <main className="flex min-h-screen items-center justify-center">
-            <Link href={'/second'}>
+            <Link href={'/'}>
                 Go to home page
             </Link>
         </main>
